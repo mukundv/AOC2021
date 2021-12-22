17505fc185c115d81e14740ecde508d13f9d0150
Mukund Venkatachar <mukund.venkatachar@gmail.com>
2021-12-22 15:05:54 +1100

    Day 7 - complete rewrite in numpy
    Signed-off-by: Mukund Venkatachar <mukund.venkatachar@gmail.com>

--- day7/day7.py
+++ day7/day7.py
@@ -1,33 +1,62 @@
-from utils import timeit
+import os
+
+import numpy as np
+from aocd import get_data
+from dotenv import load_dotenv
 
 
-def get_list(input_file_name):
-    aoc_input = []
-    for x in open(input_file_name).read().split(','):
-        aoc_input.append(int(x))
-    pos = range(min(aoc_input), max(aoc_input) + 1)
-    return aoc_input, pos
+def get_session() -> str:
+    load_dotenv()
+    return os.getenv('SESSION_COOKIE')
 
 
-@timeit
-def part1(aoc_input, pos):
-    cost = []
-    for i in pos:
-        k = sum(abs(i - j) for j in aoc_input)
-        cost.append(k)
-    return min(cost)
+def get_list(data: str = None, day: int = None, year: int = None):
+    if not data:
+        numbers = [int(x) for x in get_data(get_session(), day=day, year=year).split(',')]
+    else:
+        numbers = [int(x) for x in data.split(',')]
+    return numbers
 
 
-@timeit
-def part2(aoc_input, pos):
-    x = None
-    for i in pos:
-        j = sum(sum(range(abs(k - i) + 1)) for k in aoc_input)
-        if x is None or x > j:
-            x = j
-    return x
+def part1(numbers):
+    return get_part1_fuel(get_crabs(numbers), get_median(numbers))
 
 
-if __name__ == '__main__':
-    print(part1(*get_list("day7_input.txt")))
-    print(part2(*get_list("day7_input.txt")))
+def part2(numbers):
+    return get_part2_fuel(get_crabs(numbers), get_mean(numbers))
+
+
+def get_part2_fuel(crabs, position):
+    for crab in crabs:
+        crab["fuel"] = get_cheapest_fuel(crab, position)
+    return sum([crab["fuel"] for crab in crabs])
+
+
+def get_cheapest_fuel(crab, position):
+    return sum(range(abs(crab["position"] - position) + 1))
+
+
+def get_part1_fuel(crabs, position):
+    for crab in crabs:
+        crab["fuel"] = get_horizontal_position(crab, position)
+    return sum([crab["fuel"] for crab in crabs])
+
+
+def get_horizontal_position(crab, position):
+    return abs(crab["position"] - position)
+
+
+def get_crabs(numbers):
+    return [{"position": int(i), "fuel": 0} for i in numbers]
+
+
+def get_median(numbers):
+    return round(int(np.median(numbers)))
+
+
+def get_mean(numbers):
+    return round(int(np.mean(numbers)))
+
+
+print(f"Part 1: {part1(get_list(data=None, day=7, year=2021))}")
+print(f"Part 2: {part2(get_list(data=None, day=7, year=2021))}")
